a0f934f904dd94949c38480f2d55afe1187d1974
Ifat Chitin <ifat.chitin@gmail.com>
2014-04-16 13:36:33 +0300

    first commit, good luck to us all

--- app/src/main/java/com/hayav/HelloAndroidActivity.java
+++ app/src/main/java/com/hayav/HelloAndroidActivity.java
@@ -21,9 +21,9 @@ public class HelloAndroidActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
     }
 
 }
