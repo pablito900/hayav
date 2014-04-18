83971861fcc5aba560a40264d3e17cc3330a3eab
Ifat Chitin <ifat.chitin@gmail.com>
2014-04-19 00:15:13 +0300

    Refactor: renamed login.xml to activity_login.xml

--- app/src/main/java/com/hayav/LoginActivity.java
+++ app/src/main/java/com/hayav/LoginActivity.java
@@ -8,7 +8,7 @@ public class LoginActivity extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
     }
 
     @Override
